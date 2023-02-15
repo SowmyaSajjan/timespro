d1657c82df181efc84489d0bf4b13c7b4787e7a9
sowmyasajjan <sowmyasomu115@gmail.com>
2023-02-15 15:13:02 +0530

    commit

---
+++ javaScript/assignment _javaScript/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $("p").click(function(){
+      $(this).hide(2000).css("color","red")
+    }),
+    $("#flip").click(function(){
+        $("#div").slideDown("slow");
+    }),
+    $("ani").click(function(){
+        $("#div1").animate({left:'250px'});
+    }),
+    $("#hide").click(function(){
+        $("#callback").hide("slow",function(){
+            alert("this paragraph is now hidden");
+        });
+    }),
+    $("#text").click(function(){
+        alert($("#div2").text());
+    }),
+    $("#html").click(function(){
+        alert($("#div2").html());
+    }),
+    $("#b1").click(function(){
+        $("#name").val("Sowmya");
+    }),
+    $("#b2").click(function(){
+        $("#class").val("B.E");
+    }),
+    $("#rename").click(function(){
+        $("#name").replaceWith("Sajjan");
+    }),
+    $("#reclass").click(function(){
+        $("#class").replaceWith("12th");
+    })
+  });
+
